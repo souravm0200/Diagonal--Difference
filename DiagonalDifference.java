ea52d65d3e3e0be62e7a28b3cde116e39098f8e8
SOURAV MANDAL <sourav0200@gmail.com>
2023-04-09 00:36:42 +0600

    File created

---
+++ DiagonalDifference.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DiagonalDifference {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        int primarySum = 0;
+        int secondarySum = 0;
+        for (int i = 0; i < n; i++) {
+            primarySum += arr[i][i];
+            secondarySum += arr[i][n - 1 - i];
+        }
+        int diff = primarySum - secondarySum;
+        System.out.println(Math.abs(diff));
+    }
+}
